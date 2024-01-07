5ceeb732884272cf8fd55663bb47bcaba1245784
codespearhead <72931357+codespearhead@users.noreply.github.com>
2024-01-07 18:45:56 -0500

    feat: adiciona swift data starter

---
+++ uc11App.swift
@@ -0,0 +1,32 @@
+//
+//  uc11App.swift
+//  uc11
+//
+//  Created by user240644 on 1/7/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct uc11App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
